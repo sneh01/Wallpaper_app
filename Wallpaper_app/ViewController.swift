9d4d4d40545cad8f23b0097d9b7632a1dcd053cc
Sneh Patel <psneh1026@gmail.com>
2020-04-02 23:27:49 -0400

    Initial Commit

---
+++ Wallpaper_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wallpaper_app
+//
+//  Created by Sneh Patel on 4/2/20.
+//  Copyright © 2020 Sneh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
