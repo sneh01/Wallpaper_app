417457cc95b6b35d9f3832f7111e4091d0e7a411
Sneh Patel <psneh1026@gmail.com>
2020-04-05 18:52:19 -0400

    add UIImage for wallpaper

--- Wallpaper_app/ViewController.swift
+++ Wallpaper_app/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var firstWallpaper: UIImageView!
+    
+    
+    
+    
+    
+    
+    
 }
 
